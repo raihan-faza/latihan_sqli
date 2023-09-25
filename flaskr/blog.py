af630a028232a0141ac21cefb1772511163a7a99
raihan-faza <raihanfazasabiqr@gmail.com>
2023-09-25 12:35:46 +0700

    first commit

---
+++ flaskr/blog.py
@@ -0,0 +1,91 @@
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+from werkzeug.exceptions import abort
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'select post.id, title, body, created_at, author_id, username from post join user on post.author_id = user.id order by created_at desc'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
+
+
+@bp.route('/create', methods=['GET', 'POST'])
+@login_required
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'title required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'insert into post (title,body,author_id) values (?,?,?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+    return render_template('blog/create.html')
+
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'select post.id, title, body, created_at, author_id, username from post join user on post.author_id = user.id where post.id = ?', (
+            id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f'post with id {id} is not found')
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+    return post
+
+
+@bp.route('/<int:id>/update', methods=['POST', 'GET'])
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'title is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'update post set title = ?, body = ? where id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+    return render_template('blog/update.html', post=post)
+
+
+@bp.route('/<int:id>/delete', methods=['POST'])
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute(
+        'delete from post where id = ?',
+        (id)
+    )
+    db.commit()
+    return redirect(url_for('blog.index'))
---
+++ flaskr/database.sql
@@ -0,0 +1,15 @@
+drop table if exists user;
+drop table if exists post;
+create table user(
+  id integer primary key autoincrement,
+  username text unique not null,
+  password text not null
+);
+create table post(
+  id integer primary key autoincrement,
+  author_id integer not null,
+  created_at timestamps not null default current_timestamp,
+  title text not null,
+  body text not null,
+  foreign key (author_id) references user (id)
+);
